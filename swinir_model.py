4e857bf178022b0c5f5384f828663d5ac5510365
Vald Dia <vdidur@wisc.edu>
2025-07-24 19:13:01 -0500

    Change Name of the output

--- swinir_model.py
+++ swinir_model.py
@@ -137,9 +137,11 @@ class PureSwinIRModel(BaseModel):
             img_size=opt_net.get('img_size', 64),
             window_size=opt_net.get('window_size', 8),
             img_range=1.,
-            depths=opt_net.get('depths', [6, 6, 6, 6, 6, 6]),
+            depths=opt_net.get('depths', [6, 6, 6, 6]),
+            # depths=opt_net.get('depths', [6, 6, 6, 6, 6, 6]),
             embed_dim=opt_net.get('embed_dim', 60),
-            num_heads=opt_net.get('num_heads', [6, 6, 6, 6, 6, 6]),
+            num_heads=opt_net.get('num_heads', [6, 6, 6, 6]),
+            # num_heads=opt_net.get('num_heads', [6, 6, 6, 6, 6, 6]),
             mlp_ratio=opt_net.get('mlp_ratio', 4),
             upsampler=opt_net.get('upsampler', 'pixelshuffle'),
             resi_connection=opt_net.get('resi_connection', '3conv')
